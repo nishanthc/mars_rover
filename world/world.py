dff514c9560be59ff1b5b64e8feacde08749f401
nishanthc <nishanthchandradas@gmail.com>
2023-03-28 21:33:21 +0100

    added World class

---
+++ world/world.py
@@ -0,0 +1,31 @@
+class World:
+    """
+       Represents a grid-based world.
+
+       Attributes:
+       width (int): The width of the world.
+       height (int): The height of the world.
+
+       Methods:
+       __init__(width, height): Initializes a new instance of the World class with the given width and height.
+       """
+    def __init__(self, width: int, height: int):
+        """
+        Initializes a new instance of the World class with the given width and height.
+
+        Args:
+        width (int): The width of the world.
+        height (int): The height of the world.
+        """
+
+        # if width or height are not Integers then a TypeError is raised.
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+
+
+        self.width: int = width
+        self.height: int = height
+
+
