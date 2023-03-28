0a92be67f710d30f687518926e0c53b319efc391
nishanthc <nishanthchandradas@gmail.com>
2023-03-29 00:03:49 +0100

    added method for parising initial position and commands

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from helpers.inputs import parse_map_size
+from helpers.inputs import parse_map_size, parse_initial_position_and_commands
 from world.world import World
 
 if __name__ == "__main__":
     map_size = parse_map_size(input())
     world = World(*map_size)
+    initial_state_and_commands = parse_initial_position_and_commands(input())
+    print(initial_state_and_commands)
