72899bebfbbd26aaaf07c7cdffe70aa68ce84577
nishanthc <nishanthchandradas@gmail.com>
2023-03-28 21:55:08 +0100

    added Robot class

---
+++ robot/robot.py
@@ -0,0 +1,37 @@
+class Robot:
+    """
+    Represents a robot.
+
+    Attributes:
+    x (int): The position of the robot on the X axis.
+    y (int): The position of the robot on the Y axis.
+    H (str): The heading of the robot.
+
+    Methods:
+    __init__(x, y, heading): Initializes a new instance of the Robot class with the given x and y
+     positions and heading.
+    """
+
+    def __init__(self, x: int, y: int, heading: str):
+        """
+        Initializes a new instance of the Robot class with the given x, y and heading.
+
+        Args:
+        x (int): The position of the robot on the X axis.
+        y (int): The position of the robot on the Y axis.
+        H (str): The heading of the robot.
+        """
+
+        # if x or y are not Integers then a TypeError is raised.
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+
+        # if heading is not either N E S W then raise n ValueError.
+        if heading not in ["N", "E", "S", "W"]:
+            raise ValueError("heading must either be N E S W")
+
+        self.x: int = x
+        self.y: int = y
+        self.heading: str = heading
